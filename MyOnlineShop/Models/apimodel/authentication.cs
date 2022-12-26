4dd17e49eb6d423188db2da9ca057ac492c06dd3
mmhlego <mmhlego@gmail.com>
2022-12-26 21:23:40 +0330

    Bug fix added .gitAttributes

--- MyOnlineShop/Models/apimodel/authentication.cs
+++ MyOnlineShop/Models/apimodel/authentication.cs
@@ -12,10 +12,11 @@ namespace MyOnlineShop.Models.apimodel
         public string type { get; set; }
         public string username { get; set; }
         public string password { get; set; }
-        public string firstname { get; set; }
-        public string lastname { get; set; }
-        public int likes { get; set; }
-        public int dislikes { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public DateTime birthDate { get; set; }
     }
 }
 
